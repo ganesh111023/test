738991b5e449757d2878eaa1882be74e3a9307fb
Ganesh <ganesh071190@gmail.com>
2018-02-07 21:12:53 +0530

    minor fix

--- app/controllers/phase_controller.rb
+++ app/controllers/phase_controller.rb
@@ -7,12 +7,16 @@ class PhaseController < ApplicationController
 
 	#get_rendom_phase_path
 	def get_rendom_phase
-		id  = rand(1..10)
-		if !(session[:fetched_record].include?(id))
-			session[:fetched_record] << id
-			@phase = Phase.find_by_id id 
-		elsif (1..10).to_a.all? { |e| session[:fetched_record].include?(e) }
+		if (Phase.ids).all? { |e| session[:fetched_record].include?(e) }
 			 errors.add(:name, message: "No more element in db") 
+		else
+			id  = rand(Phase.count)
+			if !(session[:fetched_record].include?(id)) && id > 0
+				session[:fetched_record] << id
+				@phase = Phase.find_by_id id 
+			elsif (session[:fetched_record].include?(id))
+				id  = rand(Phase.count)
+			end
 		end
 	end
 
